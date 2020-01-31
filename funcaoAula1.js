7902847fd53c65a1d5c41545f7d30d6f1100ca78
Joao <joaogilberto350@hotmail.com>
2020-01-31 13:23:18 -0300

    exibindo elementos

---
+++ funcaoAula1.js
@@ -0,0 +1,22 @@
+function exibeElementoPorTag(tag){
+	$(tag).show();
+}
+
+function ocultaElementoPorTag(tag){
+	$(tag).hide();
+}
+
+function exibeElementoPorClasse(classe){
+	$('.'+ classe).show();
+}
+
+function ocultaElementoPorClasse(classe){
+	$('.'+ classe).hide();
+}
+function exibeElementoPorId(id){
+	$('#'+ id).show();
+}
+
+function ocultaElementoPorId(id){
+	$('#'+ id).hide();
+}
